b2a01943f63f3d0a1cc31658b71f92bdd9d56913
Albrecht Lohofener <albrechtloh@gmx.de>
2025-03-26 20:51:41 +0100

    Add button for LuCI

--- web-frontend/src/app/side-navigation/side-navigation.component.ts
+++ web-frontend/src/app/side-navigation/side-navigation.component.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
@@ -27,6 +28,7 @@ import { RouterModule } from '@angular/router';
     MatSlideToggleModule,
     RouterOutlet,
     RouterModule,
+    MatTooltipModule,
   ]
 })
 export class SideNavigationComponent {
@@ -37,4 +39,11 @@ export class SideNavigationComponent {
       map(result => result.matches),
       shareReplay()
     );
+
+  openLuCI() {
+    // ToDo make port 9000 configurable
+    let url = "https://" + window.location.hostname + ":9000";
+    console.log("LuCI URL: ", url);
+    window.open(url, "_blank");
+  }
 }
