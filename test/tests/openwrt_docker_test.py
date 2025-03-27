82c7ac7563ae177a99cfc6d93963ddc411571378
Albrecht Lohofener <albrechtloh@gmx.de>
2025-03-27 06:56:37 +0100

    Fix code format

--- test/tests/openwrt_docker_test.py
+++ test/tests/openwrt_docker_test.py
@@ -21,7 +21,7 @@ def parameter(request):
 # Adapt docker-compose.yml to our test needs
 @pytest.fixture(scope="session")
 def docker_compose_file(pytestconfig, parameter, docker_compose_project_name):
-        
+
     compose_file_template = os.path.join(str(pytestconfig.rootdir), "tests", "docker-compose.yml.template")
     compose_file = os.path.join(str(pytestconfig.rootdir), "tests", "docker-compose.yml.generated")
 
@@ -34,7 +34,7 @@ def docker_compose_file(pytestconfig, parameter, docker_compose_project_name):
         if len(parameter) >= 2:
             env_var = parameter[0]
             value = parameter[1]
-            if env_var != "": 
+            if env_var != "":
                 list_doc['services']['openwrt']['environment'][env_var] = value
 
         # Load volume data
@@ -44,7 +44,7 @@ def docker_compose_file(pytestconfig, parameter, docker_compose_project_name):
             data_volume_backup = parameter[2]
             vackup_dir = os.path.join(str(pytestconfig.rootdir), "docker-vackup")
             vackup = os.path.join(vackup_dir, "vackup") # Funny name ;-)
-            
+
             os.system(f"cd {vackup_dir}/../test_data/ && {vackup} import {data_volume_backup} {docker_compose_project_name}_data >/dev/null 2>/dev/null")
 
     # Save docker-compose file
@@ -65,17 +65,17 @@ def run_openwrt_shell_command(command, *arg):
     arguments += "]"
 
     # Call qemu guest tools function 'guest-exec'. This is only working if OpenWrt is booted and the qemu guest tools are running
-    process = subprocess.run(['docker','exec','openwrt','sh','-c',r"""echo -ne '{"execute":"guest-exec", "arguments": { "path": """ + command + ", \"arg\":" + arguments + r""","capture-output": true}}' | nc -w 1 -U /run/qga.sock"""], 
+    process = subprocess.run(['docker','exec','openwrt','sh','-c',r"""echo -ne '{"execute":"guest-exec", "arguments": { "path": """ + command + ", \"arg\":" + arguments + r""","capture-output": true}}' | nc -w 1 -U /run/qga.sock"""],
                          stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE, 
+                         stderr=subprocess.PIPE,
                          universal_newlines=True)
-    
+
     if process.returncode != 0:
         return None
-    
+
     if process.stdout == "":
         return None
-    
+
     # Sometime it happens that also the previous command return is included which results in two or more json strings.
     jsons = process.stdout.split('\n')
 
@@ -87,7 +87,7 @@ def run_openwrt_shell_command(command, *arg):
 
     if 'return' not in ret:
         return None
-    
+
     if 'pid' not in ret['return']:
         return None
 
@@ -96,14 +96,14 @@ def run_openwrt_shell_command(command, *arg):
     # Get stdout of process
     time.sleep(5) # Give command 5 seconds time to respond. It would be better to implement a real timeout.
     # Call qemu guest tools function 'guest-exec-status'. This is only working if OpenWrt is booted and the qemu guest tools are running
-    process = subprocess.run(['docker','exec','openwrt','sh','-c',r"""echo -ne '{"execute":"guest-exec-status", "arguments": { "pid": """ + str(pid) + r"""}}' | nc -w 1 -U /run/qga.sock"""], 
+    process = subprocess.run(['docker','exec','openwrt','sh','-c',r"""echo -ne '{"execute":"guest-exec-status", "arguments": { "pid": """ + str(pid) + r"""}}' | nc -w 1 -U /run/qga.sock"""],
                          stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE, 
+                         stderr=subprocess.PIPE,
                          universal_newlines=True)
-    
+
     if process.returncode != 0:
         return None
-    
+
     if process.stdout == "":
         return None
 
@@ -129,14 +129,14 @@ def run_openwrt_shell_command(command, *arg):
 
 def get_openwrt_info():
     # Call qemu guest tools function 'guest-get-osinfo'. This is only working if OpenWrt is booted and the qemu guest tools are running
-    process = subprocess.run(['docker','exec','openwrt','sh','-c',r"""echo -ne '{"execute":"guest-get-osinfo"}' | nc -w 1 -U /run/qga.sock"""], 
+    process = subprocess.run(['docker','exec','openwrt','sh','-c',r"""echo -ne '{"execute":"guest-get-osinfo"}' | nc -w 1 -U /run/qga.sock"""],
                          stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE, 
+                         stderr=subprocess.PIPE,
                          universal_newlines=True)
-    
+
     if process.returncode != 0:
         return None
-    
+
     if process.stdout == "":
         return None
 
@@ -151,49 +151,49 @@ def get_openwrt_info():
 
     # Assume json here, parse it
     info = json.loads(jsons[idx])
-    
+
     if 'name' not in info['return']:
         return None
 
     return info['return']
 
 def get_logs():
-    process = subprocess.run(['docker','logs','openwrt'], 
-        stdout=subprocess.PIPE, 
+    process = subprocess.run(['docker','logs','openwrt'],
+        stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         universal_newlines=True)
-    
+
     return process.stdout
 
 def get_container_status():
-    process = subprocess.run(['docker','inspect','openwrt'], 
-        stdout=subprocess.PIPE, 
+    process = subprocess.run(['docker','inspect','openwrt'],
+        stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         universal_newlines=True)
-    
+
     ret = json.loads(process.stdout)
     status = ret[0]['State']['Status']
 
     if(status != 'running'):
         raise Exception(f"Container not running. Status '{status}'")
-    
+
     return True
 
 def is_openwrt_booted():
     if get_container_status() == False:
         return False
-    
+
     service_status = get_openwrt_info()
 
     if service_status != None:
         return True
     else:
         return False
-    
+
 def is_specific_log(log_text):
     if get_container_status() == False:
         return False
-    
+
     return log_text in get_logs()
 
 def is_container_running():
@@ -205,7 +205,7 @@ def is_container_running():
         if response.status_code == 200:
             return True
         else:
-         return False   
+         return False
     except Exception:
         return False
 
@@ -257,12 +257,12 @@ def test_openwrt_booted(docker_services):
     assert 'OpenWrt' == info['name']
 
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('LAN_IF','veth'),('LAN_IF','veth,nofixedip'),('LAN_IF','ens5'),('LAN_IF',''),('LAN_IF','host')], indirect=True,
     ids=['LAN_IF=veth','LAN_IF=veth,nofixedip', 'LAN_IF=ens5', 'LAN_IF=""', 'LAN_IF="host"'])
 def test_openwrt_lan(docker_services, parameter):
     wait_for_openwrt_startup(docker_services)
-    
+
     match parameter[1]:
         case 'veth':
             try:
@@ -272,12 +272,12 @@ def test_openwrt_lan(docker_services, parameter):
             return
 
         case 'veth,nofixedip':
-            # Get all IPv4 addresses 
-            process = subprocess.run(['sh','-c',r'ip addr show veth-openwrt0 | grep -oP "(?<=inet\s)\d+(\.\d+){3}"'], 
-                stdout=subprocess.PIPE, 
+            # Get all IPv4 addresses
+            process = subprocess.run(['sh','-c',r'ip addr show veth-openwrt0 | grep -oP "(?<=inet\s)\d+(\.\d+){3}"'],
+                stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT,
                 universal_newlines=True)
-    
+
             # No IP address shall be found
             assert ('' in process.stdout) == True
             return
@@ -290,23 +290,23 @@ def test_openwrt_lan(docker_services, parameter):
             response = run_openwrt_shell_command("ping", "-c1", "-W2", "-w2", "192.168.1.2")
             assert response['exitcode'] == 0
             return
-        
+
         case _: # Usage of real Ethernet interface e.g. 'eth0'
             # This test is most likely only working in a github action environment because multiple VM are necessary to test it. See the action file, please.
             # Try to ping LAN-VM
             response = run_openwrt_shell_command("ping", "-c1", "-W2", "-w2", "172.31.1.2")
             assert response['exitcode'] == 0
             return
-    
+
     assert False, 'Unknown parameter'
 
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('WAN_IF','host'),('WAN_IF','none'),('WAN_IF','ens4'),('WAN_IF','')], indirect=True,
     ids=['WAN_IF=host', 'WAN_IF=none', 'WAN_IF=ens4', 'WAN_IF=""'])
 def test_openwrt_wan(docker_services, parameter):
     wait_for_openwrt_startup(docker_services)
-    
+
     match parameter[1]:
         case '' | 'host':
             # For some reason ping is not working at github actions, so use nslookup to test internet connection
@@ -315,13 +315,13 @@ def test_openwrt_wan(docker_services, parameter):
             except polling2.TimeoutException:
                 assert True, 'nslookup timeout'
             return
-        
+
         case 'none':
             # We are looking for eth1. It should not existing
-            response = run_openwrt_shell_command("ip", "addr")        
+            response = run_openwrt_shell_command("ip", "addr")
             assert ('eth1' in response['out-data']) == False
             return
-        
+
         case _: # Usage of real Ethernet interface e.g. 'eth0'
             # This test is most likely only working in a github action environment because multiple VM are necessary to test it. See the action file, please
             # Try to get IP address from WAN-VM
@@ -336,7 +336,7 @@ def test_openwrt_wan(docker_services, parameter):
 
     assert False, 'Unknown parameter'
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('FORWARD_LUCI','true'),('FORWARD_LUCI','false')], indirect=True,
     ids=['FORWARD_LUCI=true', 'FORWARD_LUCI=false'])
 def test_nginx_luci_forwarding_access(docker_services, parameter):
@@ -350,21 +350,21 @@ def test_nginx_luci_forwarding_access(docker_services, parameter):
             #print(get_logs())
             #os.system("ip addr")
             #os.system("ping -c 1 172.31.1.1")
-            
+
             #print(run_openwrt_shell_command("ip", "addr")['out-data'])
             #print(run_openwrt_shell_command("ping", "-c1", "-W2", "-w2", "172.31.1.2")['out-data'])
-            
+
             pytest.fail(f"Unexpected exception raised: {excinfo}")
 
     if parameter[1] == 'false':
         try:
             response = requests.get("https://localhost:9000", verify=False)
             pytest.fail(f'LuCI forwarding is active')
-        except Exception as excinfo: 
+        except Exception as excinfo:
             return
 
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('CPU_COUNT',1),('CPU_COUNT',2)], indirect=True,
     ids=['CPU_COUNT=1', 'CPU_COUNT=2'])
 def test_cpu_num(docker_services, parameter):
@@ -375,7 +375,7 @@ def test_cpu_num(docker_services, parameter):
     assert parameter[1] == cpu_num
 
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('','','20240922_test_volume_openwrt_23.05.4_ext4'),('','','20241114_test_volume_openwrt_23.05.5')], indirect=True,
     ids=['20240922_test_volume_openwrt_23.05.4 ext4', '20241114_test_volume_openwrt_23.05.5'])
 def test_openwrt_migrate_existing_volume(docker_services):
@@ -385,7 +385,7 @@ def test_openwrt_migrate_existing_volume(docker_services):
     assert ('upgrade: Restoring config files' in get_logs()) == True
 
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('','','20240922_test_volume_openwrt_23.05.4_ext4'),('','','20241114_test_volume_openwrt_23.05.5')], indirect=True,
     ids=['20240922_test_volume_openwrt_23.05.4', '20241114_test_volume_openwrt_23.05.5'])
 def test_openwrt_migrate_settings(docker_services):
@@ -393,7 +393,7 @@ def test_openwrt_migrate_settings(docker_services):
 
     # Here we test a custom configuration that is stored in the existing volume.
     # Currently hostname and an additional interface are configured.
-    
+
     # Check for host name 'TestRouter'
     response = run_openwrt_shell_command("uci", "get", "system.@system[0].hostname")
     assert ('TestRouter' in response['out-data']) == True
@@ -444,7 +444,7 @@ def test_api_get_openwrt_info(docker_services):
     assert ('{"command":"/run/qemu_qmp.sh [\\"-V\\"]' in response.content.decode()) == True
 
 
-def test_api_get_container_info(docker_services):  
+def test_api_get_container_info(docker_services):
     wait_for_container_startup(docker_services)
     response = requests.get("http://localhost:8006/api/get_container_info")
     assert ('Content of /var/vm/' in response.content.decode()) == True
@@ -465,7 +465,7 @@ def test_mdns(docker_services):
     return
 
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('WAN_IF','host'),('WAN_IF','')], indirect=True,
     ids=['WAN_IF=host', 'WAN_IF=""'])
 def test_port_8000_luci(docker_services):
@@ -473,12 +473,12 @@ def test_port_8000_luci(docker_services):
 
     response = run_openwrt_shell_command("fw_wan_open_http", "")
     assert response['exitcode'] == 0
-    
+
     response = requests.get("http://localhost:8000")
     assert ('LuCI - Lua Configuration Interface' in response.content.decode()) == True
 
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('WAN_IF','host'),('WAN_IF','')], indirect=True,
     ids=['WAN_IF=host', 'WAN_IF=""'])
 def test_port_8022_ssh(docker_services):
@@ -486,16 +486,16 @@ def test_port_8022_ssh(docker_services):
 
     response = run_openwrt_shell_command("fw_wan_open_ssh", "")
     assert response['exitcode'] == 0
-    
-    process = subprocess.run(['timeout','2','ssh','root@localhost','-p 8022','-oStrictHostKeyChecking=no'], 
-        stdout=subprocess.PIPE, 
+
+    process = subprocess.run(['timeout','2','ssh','root@localhost','-p 8022','-oStrictHostKeyChecking=no'],
+        stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         universal_newlines=True)
-    
+
     assert ('OpenWrt' in process.stdout) == True
 
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('OPENWRT_AFTER_BOOT_CMD','a=$$((1+1));echo The result of 1+1 is $${a}')], indirect=True,
     ids=['OPENWRT_AFTER_BOOT_CMD="a=$$((1+1));echo The result of 1+1 is $${a}"'])
 def test_openwrt_after_boot_cmd(docker_services):
@@ -503,12 +503,12 @@ def test_openwrt_after_boot_cmd(docker_services):
     wait_for_specific_log(docker_services, 'Detected OpenWrt is booted')
 
     wait_for_specific_log(docker_services, 'Command "a=$((1+1));echo The result of 1+1 is ${a}" exit successfully')
-   
+
     logs = get_logs()
     assert ('The result of 1+1 is 2' in logs) == True
 
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('PCI_1','00:05.0')], indirect=True,
     ids=['PCI_1="00:05.0"'])
 def test_pci_passthrough_fail(docker_services):
@@ -525,7 +525,7 @@ def test_pci_passthrough_fail(docker_services):
     assert ('ERROR: Failed to bind PCI device 0000:00:05.0 to vfio-pci.' in get_logs()) == True
 
 
-@pytest.mark.parametrize("parameter", 
+@pytest.mark.parametrize("parameter",
     [('DISABLE_OPENWRT_AUTO_UPGRADE','true','20241114_test_volume_openwrt_23.05.5')], indirect=True,
     ids=['DISABLE_OPENWRT_AUTO_UPGRADE="true" with 20241114_test_volume_openwrt_23.05.5'])
 def test_disable_openwrt_auto_upgrade(docker_services):
