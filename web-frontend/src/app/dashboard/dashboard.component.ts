4f33c571a67d7b6af6f613b43f997e0cd093f678
Albrecht Lohofener <albrechtloh@gmx.de>
2025-04-01 12:04:52 +0200

    Remove unused module

--- web-frontend/src/app/dashboard/dashboard.component.ts
+++ web-frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
