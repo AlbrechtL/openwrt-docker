f30ef2f49e4c0f337b16351ea028d02c0bf27c10
Albrecht Lohofener <albrechtloh@gmx.de>
2025-04-12 09:23:15 +0200

    Add option IMAGE_SIZE_ON_INIT

--- src/install_openwrt_rootfs.sh
+++ src/install_openwrt_rootfs.sh
@@ -11,11 +11,24 @@ FILE=/storage/squashfs-combined-${OPENWRT_IMAGE_ID}.img
 if [[ ($DISABLE_OPENWRT_AUTO_UPGRADE != "true") ||  (! -f /storage/current_version) ]]; then
   if [ -f "$FILE" ]; then
       info "$FILE exists. Nothing to do."
-  else 
+  else
       info "$FILE does not exist. Copying squashfs-combined-${OPENWRT_VERSION}.img to storage ..."
       cp /var/vm/squashfs-combined-${OPENWRT_VERSION}.img.gz /storage/squashfs-combined-${OPENWRT_IMAGE_ID}.img.gz
       gzip -d /storage/squashfs-combined-${OPENWRT_IMAGE_ID}.img.gz
-      
+
+      # Check if IMAGE_SIZE_ON_INIT is set
+      if [[ -n "$IMAGE_SIZE_ON_INIT" ]]; then
+          # Check if IMAGE_SIZE_ON_INIT is greater than 511
+          if [[ "$IMAGE_SIZE_ON_INIT" -gt 511 ]]; then
+              info "Resize OpenWrt to $IMAGE_SIZE_ON_INIT MB"
+              dd if=/dev/zero of="/storage/squashfs-combined-${OPENWRT_IMAGE_ID}.img" seek="$IMAGE_SIZE_ON_INIT" obs=1MB count=0
+          else
+              error "Error: IMAGE_SIZE_ON_INIT must be greater than 511."
+              rm /storage/squashfs-combined-${OPENWRT_IMAGE_ID}.img
+              exit 1
+          fi
+      fi
+
       if [ -f "/storage/current_version" ]; then
         mv /storage/current_version /storage/old_version
       fi
